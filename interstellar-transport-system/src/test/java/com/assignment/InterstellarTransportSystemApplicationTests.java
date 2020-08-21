e82991ff7f672bf4e2bc3c73cdcd385a0305ca37
ipokunzi <peter.okunzi@gmail.com>
2020-08-21 13:43:53 +0200

    Add files via upload

--- interstellar-transport-system/src/test/java/com/assignment/InterstellarTransportSystemApplicationTests.java
+++ interstellar-transport-system/src/test/java/com/assignment/InterstellarTransportSystemApplicationTests.java
@@ -1,6 +1,5 @@
 package com.assignment;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,6 +16,8 @@ import com.assignment.InterstellarTransportSystemApplication;
 import com.assignment.model.Planet;
 import com.assignment.model.Route;
 import com.assignment.service.PlanetService;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = InterstellarTransportSystemApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class InterstellarTransportSystemApplicationTests {
@@ -43,16 +44,22 @@ class InterstellarTransportSystemApplicationTests {
         Planet planet = new Planet();
         planet.setNode("A");
         planet.setName("Earth");
-
+       
         ResponseEntity<Planet> postResponse = restTemplate.postForEntity(getRootUrl() + "/api/v1/planets", planet, Planet.class);
         
         planet.setNode("B");
         planet.setName("Moon");
 
         postResponse = restTemplate.postForEntity(getRootUrl() + "/api/v1/planets", planet, Planet.class);
+      
+        planet.setNode("C");
+        planet.setName("Jupiter");
+
+        postResponse = restTemplate.postForEntity(getRootUrl() + "/api/v1/planets", planet, Planet.class);
         
-        Assert.assertNotNull(postResponse);
-        Assert.assertNotNull(postResponse.getBody());
+        assertNotNull(postResponse);
+        assertNotNull(postResponse.getBody());
+        assertEquals(201, postResponse.getStatusCodeValue());
     }
 	
 	@Test
@@ -63,134 +70,103 @@ class InterstellarTransportSystemApplicationTests {
          ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/api/v1/planets",
          HttpMethod.GET, entity, String.class);
   
-         Assert.assertNotNull(response.getBody());
+         assertNotNull(response.getBody());
     }
 	
     @Test
     public void testGetPlanetById() {
         Planet planet = restTemplate.getForObject(getRootUrl() + "/api/v1/planets/1", Planet.class);
-        Assert.assertNotNull(planet);
+        assertNotNull(planet);
     }
 
     @Test
-    public void testUpdatePost() {
-         int id = 1;
+    public void testUpdatePlanetPost() {
+         int id = 3;
          Planet planet = restTemplate.getForObject(getRootUrl() + "/api/v1/planets/" + id, Planet.class);
-         planet.setNode("AA");
-         planet.setName("Earth Earth");
+         planet.setNode("CC");
+         planet.setName("Jupiter Jupiter");
 
          restTemplate.put(getRootUrl() + "/api/v1/planets/" + id, planet);
 
          Planet updatedPlanet = restTemplate.getForObject(getRootUrl() + "/api/v1/planets/" + id, Planet.class);
-         Assert.assertNotNull(updatedPlanet);
+         assertNotNull(updatedPlanet);
     }
     
+//    @Test
+//    public void testDeletePlanetPost() {
+//         int id = 3;
+//         Planet planet = restTemplate.getForObject(getRootUrl() + "/api/v1/planets/" + id, Planet.class);
+//         assertNotNull(planet);
+//
+//         restTemplate.delete(getRootUrl() + "/api/v1/planets/" + id);
+//         try {
+//              planet = restTemplate.getForObject(getRootUrl() + "/api/v1/planets/" + id, Planet.class);
+//         } catch (final HttpClientErrorException e) {
+//        	  assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
+//         }
+//    }
+              
     @Test
-    public void testDeletePlanetPost() {
-         int id = 1;
-         Planet planet = restTemplate.getForObject(getRootUrl() + "/api/v1/planets/" + id, Planet.class);
-         Assert.assertNotNull(planet);
+    public void testCreateRoute() {
+    	Route route = new Route();
+        route.setRouteid(1);
+        route.setSource(planetService.getPlanetByNode("A"));
+        route.setDestination(planetService.getPlanetByNode("B"));
+        route.setDistance(0.11);
+        route.setTraffic(0.22);
 
-         restTemplate.delete(getRootUrl() + "/api/v1/planets/" + id);
-    
-         try {
-              planet = restTemplate.getForObject(getRootUrl() + "/api/v1/planets/" + id, Planet.class);
-         } catch (final HttpClientErrorException e) {
-        	  Assert.assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
-         }
+        ResponseEntity<Route> postResponse = restTemplate.postForEntity(getRootUrl() + "/api/v1/routes", route, Route.class);
+        assertNotNull(postResponse);
+        assertNotNull(postResponse.getBody());
     }
-         
-    @Test
-    public void testDeletePlanetPostError() {
-         int id = 2;
-         Planet planet = restTemplate.getForObject(getRootUrl() + "/api/v1/planets/" + id, Planet.class);
-         Assert.assertNotNull(planet);
-
-         restTemplate.delete(getRootUrl() + "/api/v1/planets/" + id);
-    
-         try {
-              planet = restTemplate.getForObject(getRootUrl() + "/api/v1/planets/" + id, Planet.class);
-         } catch (final HttpClientErrorException e) {
-        	  Assert.assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
-         }
-     }
-         
-         @Test
-         public void testCreateRoute() {
-             Route route = new Route();
-             route.setRouteid(1);
-             route.setSource(planetService.getPlanetByNode("A"));
-             route.setDestination(planetService.getPlanetByNode("B"));
-             route.setDistance(0.11);
-             route.setTraffic(0.22);
-
-             ResponseEntity<Route> postResponse = restTemplate.postForEntity(getRootUrl() + "/api/v1/routes", route, Route.class);
-             Assert.assertNotNull(postResponse);
-             Assert.assertNotNull(postResponse.getBody());
-         }
      	
-     	@Test
-         public void testGetAllRoutes() {
-              HttpHeaders headers = new HttpHeaders();
-              HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+    @Test
+    public void testGetAllRoutes() {
+    	HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<String>(null, headers);
 
-              ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/api/v1/routes",
-              HttpMethod.GET, entity, String.class);
+        ResponseEntity<String> response = restTemplate.exchange(getRootUrl() + "/api/v1/routes",
+        HttpMethod.GET, entity, String.class);
        
-              Assert.assertNotNull(response.getBody());
-         }
+        assertNotNull(response.getBody());
+    }
      	
-         @Test
-         public void testGetRouteById() {
-             Route route = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/1", Route.class);
-             Assert.assertNotNull(route);
-         }
+    @Test
+    public void testGetRouteById() {
+    	Route route = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/1", Route.class);
+        assertNotNull(route);
+    }
 
-         @Test
-         public void testRoutePost() {
-              int id = 1;
-              Route route = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/" + id, Route.class);
-              
-              route.setRouteid(1);
-              route.setSource(planetService.getPlanetByNode("A"));
-              route.setDestination(planetService.getPlanetByNode("B"));
-              route.setDistance(10.11);
-              route.setTraffic(10.22);
+    @Test
+    public void testUpdateRoutePost() {
+    	int id = 1;
+        Route route = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/" + id, Route.class);
+             
+        route.setRouteid(1);
+        route.setSource(planetService.getPlanetByNode("A"));
+        route.setDestination(planetService.getPlanetByNode("B"));
+        route.setDistance(10.11);
+        route.setTraffic(10.22);
 
-              restTemplate.put(getRootUrl() + "/api/v1/routes/" + id, route);
+        restTemplate.put(getRootUrl() + "/api/v1/routes/" + id, route);
 
-              Route updatedRoute = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/" + id, Route.class);
-              Assert.assertNotNull(updatedRoute);
-         }
+        Route updatedRoute = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/" + id, Route.class);
+        assertNotNull(updatedRoute);
+    }
          
-         @Test
-         public void testDeleteRoutePost() {
-              int id = 1;
-              Route route = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/" + id, Route.class);
-              Assert.assertNotNull(route);
-
-              restTemplate.delete(getRootUrl() + "/api/v1/routes/" + id);
-         
-              try {
-                   route = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/" + id, Route.class);
-              } catch (final HttpClientErrorException e) {
-             	  Assert.assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
-              }
-         }
-              
-         @Test
-         public void testDeleteRoutePostError() {
-              int id = 2;
-              Route route = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/" + id, Route.class);
-              Assert.assertNotNull(route);
-
-              restTemplate.delete(getRootUrl() + "/api/v1/routes/" + id);
-         
-              try {
-                   route = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/" + id, Route.class);
-              } catch (final HttpClientErrorException e) {
-             	  Assert.assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
-          }
-  }
-
+//    @Test
+//    public void testDeleteRoutePost() {
+//    	int id = 1;
+//        Route route = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/" + id, Route.class);
+//        assertNotNull(route);
+//
+//        restTemplate.delete(getRootUrl() + "/api/v1/routes/" + id);
+//         
+//        try {
+//          	route = restTemplate.getForObject(getRootUrl() + "/api/v1/routes/" + id, Route.class);
+//        } catch (final HttpClientErrorException e) {
+//        	assertEquals(e.getStatusCode(), HttpStatus.NOT_FOUND);
+//        }
+//    }
+	
 }
